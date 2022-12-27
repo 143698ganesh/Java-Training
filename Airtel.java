a2eaa7c7ac1b059cb93cd0694452cad55b3ade74
143698ganesh <121091321+143698ganesh@users.noreply.github.com>
2022-12-27 22:50:35 +0530

    Spring example

---
+++ Airtel.java
@@ -0,0 +1,16 @@
+package training;
+
+public class Airtel implements Sim {
+
+	public void calling() {
+		System.out.println("using airtel for calling");
+		
+	}
+
+	public void data() {
+		System.out.println("using airtel for data");
+		
+		
+	}
+
+}
---
+++ Jio.java
@@ -0,0 +1,18 @@
+package training;
+
+public class Jio implements Sim {
+
+	public void calling() {
+		System.out.println("using jio for calling");
+		
+		
+	}
+
+	public void data() {
+		System.out.println("using ji for data");
+		
+		
+	}
+	
+
+}
